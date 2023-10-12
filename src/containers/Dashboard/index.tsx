79dcd6f6d975b9307ec80269f9ce095cb15155a7
Muhammad Akmal <akmalfakhrib@gmail.com>
2023-10-12 17:02:35 +0700

    Create new container dashboard

---
+++ src/containers/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from '@rneui/base';
+import {View} from 'react-native';
+
+const Dashboard = () => {
+  return (
+    <View>
+      <Text>
+        Uji coba
+      </Text>
+    </View>
+  )
+};
+
+export default Dashboard;
